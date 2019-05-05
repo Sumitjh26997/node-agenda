31b9176df930b0203f75225f586ed21280676e8d
Sumitjh26997 <sumit.hotchandani@gmail.com>
2019-05-05 10:09:46 +0530

    Transaction not working

--- agenda.js
+++ agenda.js
@@ -3,7 +3,7 @@ const web3 = new Web3("http://127.0.0.1:7545");
 const Tx = require("ethereumjs-tx");
 
 const user = '0xEe70a0B2E5fb9B3fBFF4e691c5e4a53EC317aC87';
-const u_priv = 'db08847639154e2df55a233145fe5391981b8804b50b237b2489650505f4e54d';
+const u_priv = Buffer('db08847639154e2df55a233145fe5391981b8804b50b237b2489650505f4e54d','hex');
 const abi = [{"constant":false,"inputs":[{"name":"_premium","type":"uint256"}],"name":"setPremium","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getState","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_reason","type":"string"}],"name":"requestRecords","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"recordID","type":"uint256"},{"name":"_masterkey","type":"string"}],"name":"getRecordsRenewal","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"policyGrace","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_coverage","type":"uint256"}],"name":"renewPolicy","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"confirmRenewal","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[],"name":"applyPolicy","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"recordID","type":"uint256"},{"name":"_masterkey","type":"string"}],"name":"getRecordsApplied","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getRecords","outputs":[{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renewPolicy","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"state","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getPremium","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"policyLapse","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"confirmPolicy","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[],"name":"policyInactive","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getDetails","outputs":[{"name":"","type":"address"},{"name":"","type":"address"},{"name":"","type":"uint256"},{"name":"","type":"uint8"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"string"},{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"prevState","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"contractBuyer","type":"address"},{"name":"contractSeller","type":"address"},{"name":"_coverage","type":"uint256"}],"payable":true,"stateMutability":"payable","type":"constructor"}];
 
 const recv = '0xa577C9d2BA3bB4aD1e09df6dFf697Cb3A639eB7a';
@@ -16,11 +16,10 @@ async function getTransactionCount(data){
   const worker = data.worker
   console.log("Inside: ",contract,worker)
   //await web3.eth.getBlockNumber().then(console.log);
-  await web3.eth.getTransactionCount(worker, (err, txCount) => {
+  await web3.eth.getTransactionCount(worker, async (err, txCount) => {
     var data = new web3.eth.Contract(abi, contract);
-   // console.log(data);
-    console.log(data.methods.applyPolicy())
-    var x = data.methods.applyPolicy()
+    var x = data.methods.applyPolicy().encodeABI();
+    console.log("ABI:",x)
     const txObject = {
       nonce : txCount,
       to : contract,   
@@ -35,9 +34,9 @@ async function getTransactionCount(data){
 
   const serializedTransaction = tx.serialize();
   const raw = "0x" + serializedTransaction.toString("hex");
-
+  console.log("raw:",raw);
   //Broadcast a transaction
-  web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+  await web3.eth.sendSignedTransaction(raw, (err, txHash) => {
     console.log("txHash : ", txHash);
   });
   });
@@ -68,7 +67,7 @@ async function run() {
   // Schedule a job for 1 second from now and persist it to mongodb.
   // Jobs are uniquely defined by their name, in this case "hello"
 
-  agenda.schedule(new Date(Date.now() + 3000), 'transact',{contract : recv, worker : user});
+  agenda.schedule(new Date(Date.now() + 10000), 'transact',{contract : recv, worker : user});
   agenda.start();
 }
 
